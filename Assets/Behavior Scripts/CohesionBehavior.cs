6fa9dba4ca39978905867fcd48556e34fe4577db
MoistGoolem <olra0312@gmail.com>
2023-06-03 14:21:28 +0200

    Add boids and find coherent boids

---
+++ Assets/Behavior Scripts/CohesionBehavior.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behavior/Cohesion")]
+public class CohesionBehavior : FlockBehavior {
+
+    public override Vector2 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock) {
+        
+    }
+}
