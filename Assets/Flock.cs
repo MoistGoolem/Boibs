1e0ea5445e243865aff63337e0e1a42349ed2ae2
MoistGoolem <olra0312@gmail.com>
2023-06-03 14:30:19 +0200

    Added cohesion bahavior

--- Assets/Flock.cs
+++ Assets/Flock.cs
@@ -53,15 +53,14 @@ public class Flock : MonoBehaviour {
             List<Transform> context = GetNearbyObjects(agent);
 
             //Only for testing
-            agent.GetComponentInChildren<SpriteRenderer>().color = Color.Lerp(Color.white, Color.red, context.Count / 6f);
-            /*
+            //agent.GetComponentInChildren<SpriteRenderer>().color = Color.Lerp(Color.white, Color.red, context.Count / 6f);
+            
             Vector2 move = behavior.CalculateMove(agent, context, this);
             move *= driveFactor;
             if(move.sqrMagnitude > squareMaxSpeed) {
                 move = move.normalized * maxSpeed;
             }
-            agent.Move(move)
-            */
+            agent.Move(move);
         }
     }
 
