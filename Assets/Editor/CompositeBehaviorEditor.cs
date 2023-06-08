be75d62dfbcaa66da41b53eb2ac039bb95720a96
Oliver Ramstedt <ora@capasystems.com>
2023-06-08 10:51:16 +0200

    Setup basic editor and added weights and behaviors

--- Assets/Editor/CompositeBehaviorEditor.cs
+++ Assets/Editor/CompositeBehaviorEditor.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class CompositeEditor : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+[CustomEditor(typeof(CompositeBehavior))]
+public class CompositeEditor : Editor {
+    public override void OnInspectorGUI() {
+        //setup inspector
+        CompositeBehavior cb = (CompositeBehavior)target;
         
-    }
+        Rect r = EditorGUILayout.BeginHorizontal();
+        r.height = EditorGUIUtility.singleLineHeight;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //check for behaviors
+        if(cb.behaviors == null || cb.behaviors.Length == 0) {
+            EditorGUILayout.HelpBox("No behaviors in array.", MessageType.Warning);
+            EditorGUILayout.EndHorizontal();
+            r = EditorGUILayout.BeginHorizontal();
+            r.height = EditorGUIUtility.singleLineHeight;
+        } else {
+            r.x = 30f;
+            r.width = EditorGUIUtility.currentViewWidth - 95f;
+            EditorGUI.LabelField(r, "Behaviors");
+            r.x = EditorGUIUtility.currentViewWidth - 65f;
+            r.width = 60f;
+            EditorGUI.LabelField(r, "Weights");
+            r.y += EditorGUIUtility.singleLineHeight * 1.2f;
+        }
     }
 }
